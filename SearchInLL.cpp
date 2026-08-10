09e45b344526d1c24cc5b5d7484e550d5a220f98
Purva nigade <purvanigade6@gmail.com>
2026-08-10 12:34:13 +0530

    Add linked list creation and search functions
    Implement linked list creation and search functionality.

---
+++ SearchInLL.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+class Node{
+    public: 
+    int data; 
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data = data1; 
+        next = next1; 
+    }
+    public: 
+    Node(int data1){
+        data = data1; 
+        next = nullptr; 
+    }
+}; 
+
+Node* convertArr2LL(vector<int>& arr){
+    Node* head = new Node(arr[0]); 
+    Node* Mover = head; 
+    for(int i = 1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        Mover->next = temp; 
+        Mover = temp; 
+    }
+    return head; 
+}
+int ChcekIfPresent(Node* head,int val){
+    Node* temp = head; 
+    while(temp){
+        if(temp->data == val)return 1; 
+        temp = temp->next; 
+    }
+    return 0; 
+}
+
+int main(){
+    vector<int> arr = {3,8,4,7,10}; 
+    Node* head = convertArr2LL(arr); 
+    cout<<ChcekIfPresent(head,10); 
+}
