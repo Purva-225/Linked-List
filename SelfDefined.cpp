e8eee868b2b70f1acdab04e4b85d655960a9426e
Purva nigade <purvanigade6@gmail.com>
2026-08-10 11:14:54 +0530

    Add initial structure for SelfDefined.cpp

---
+++ SelfDefined.cpp
@@ -0,0 +1,26 @@
+// SELF DEFINED DATA STRUCTURE: 
+
+#include <bits/stdc++.h>
+using namespace std; 
+
+class Node{
+    public:
+    int data; 
+    Node* next; 
+    
+    public:
+    Node(int data1,Node* next1){
+        data = data1; 
+        next = next1; 
+    }
+    public: 
+    Node(int data1){
+        data = data1; 
+        next = nullptr; 
+    }
+};
+int main(){
+    vector<int> arr = {2,5,8,9}; 
+    Node* y = new Node(arr[0]);
+    cout<<y;
+}
