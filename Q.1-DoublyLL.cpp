8b52adc27df64e1c1e7c351a01f2113ed1b03e3b
Purva nigade <purvanigade6@gmail.com>
2026-08-18 11:55:09 +0530

    Update Q.1-DoublyLL.cpp

--- Q.1-DoublyLL.cpp
+++ Q.1-DoublyLL.cpp
@@ -29,6 +29,7 @@ Node* ConvertArr2DLL(vector<int> &arr){
     for(int i = 1; i<arr.size(); i++){
         Node* temp = new Node(arr[i], nullptr, prev); 
         prev->next = temp; 
+        temp->prev = prev; 
         prev = temp; 
     }
     return head; 
@@ -45,3 +46,33 @@ int main(){
    print(head); 
     return 0; 
 }
+
+// From Greeks
+
+/* class Node {
+  public:
+    int data;
+    Node* next;
+    Node* prev;
+    Node(int d) {
+        data = d;
+        next = nullptr;
+        prev = nullptr;
+    }
+}; */
+
+class Solution {
+  public:
+    Node* createDLL(vector<int>& arr) {
+        Node* head = new Node(arr[0]);
+       Node* prev = head; 
+       
+       for(int i = 1; i<arr.size(); i++){
+           Node* temp = new Node(arr[i]);
+            prev->next = temp; 
+            temp->prev = prev; 
+            prev = temp; 
+       }
+       return head; 
+    }
+};
